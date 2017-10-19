b4d44010853112d8ab363c53f0f4fed601d01295
Jonathan Feldman <jf44122@gmail.com>
2017-10-19 10:38:47 -0400

    HackerRank Updates HackerRank in a string

--- HackerRankInAString/Program.cs
+++ HackerRankInAString/Program.cs
@@ -1,15 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace HackerRankInAString
+class Solution
 {
-    class Program
+
+    static void Main(String[] args)
     {
-        static void Main(string[] args)
+        int q = Convert.ToInt32(Console.ReadLine());
+        for (int a0 = 0; a0 < q; a0++)
         {
+            string s = Console.ReadLine();
+            char[] ss = s.ToCharArray();
+            string hackerRank = "hackerrank";
+            string x = IsItThere(s, hackerRank);
+            Console.WriteLine(x);
         }
+    }
+    public static string IsItThere(string ss, string thisstring)
+    {
+        if (ss.Length < thisstring.Length)
+            return "NO";
+
+        string usable = thisstring;
+        for (int ccc = 0; ccc < ss.Length; ccc++)
+        {
+            if (ss[ccc] == usable[0])
+            {
+                usable = usable.Remove(0, 1);
+                if (string.IsNullOrEmpty(usable))
+                    return "YES";
+            }
+        }
+        return "NO";
     }
 }
